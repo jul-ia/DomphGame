1bf6491030e8e2c247a6ef2560af0a24d5b3f863
Inna <1inna.balabon@gmail.com>
2019-02-10 20:29:01 +0200

    Screensaver class + TestResourses

---
+++ DomphGame_v1/DomphGame_v1/Classes/Screensaver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace DomphGame_v1.Classes
+{
+    public class Screensaver
+    {
+        List<BitmapImage> images;
+        int IdCurrentGame;
+        public Screensaver(int id)
+        {
+            images = new List<BitmapImage>();
+            IdCurrentGame = id;
+        }
+
+        private void AddImage()
+        {
+            images.Add(new BitmapImage(new Uri(@"..\..\Resourser\101.png")));
+        }
+
+        public BitmapImage GetImage(int index)
+        {
+            foreach (var item in images)
+            {
+                if (item.ToString() == index.ToString())
+                    return item;
+            }
+            return null;
+        }
+
+        public void SmthTest()
+        {
+           // if()
+        }
+    }
+}
